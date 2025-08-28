adb19d25d67da4ccbb56ebf851758e11d8046f8e
laiwola <layiibrahim32@gmail.com>
2025-08-28 22:33:13 +0100

    final2_update

--- voiceAI/evaluate.py
+++ voiceAI/evaluate.py
@@ -1,82 +1,10 @@
-# from train import VoiceModel, device
-# import torch
-# import nltk
-# from nltk.tokenize import word_tokenize
-# from nltk.stem import WordNetLemmatizer
-
-# # Download required NLTK data (if not already)
-# nltk.download("punkt")
-# nltk.download("wordnet")
-
-# word_mapping=torch.load('word_dict.pth')
-# word_in_index=word_mapping['word_in_index']
-# word_out_index=word_mapping['word_out_index']
-# index_out_word=word_mapping['index_out_word']
-# index_in_word=word_mapping['index_in_word']
-# len_input=len(word_in_index)
-# len_output=len(word_out_index)
-
-
-
-
-# model = VoiceModel(
-#     input_vocab=len_input,
-#     output_vocab=len_output,
-#     hidden=256,
-#     num_layer=2,
-#     num_embedd=128
-# ).to(device)
-
-# model.load_state_dict(torch.load('voice_model.pth',map_location=device))
-# model.eval()
-# lemmatizer=WordNetLemmatizer()
-
-# def process_input(input):
-#     # tokenize the input
-#     tokenize_inp=word_tokenize(input.lower())
-#     # lemmatize the input
-#     tokenize_inp=[lemmatizer.lemmatize(tok)for tok in tokenize_inp]
-#     # convert token to indices, unknow words to unk if it exist
-#     unk_idx=word_in_index.get("<UNK>")
-#     indices=[word_in_index.get(tok,unk_idx)for tok in tokenize_inp if tok in word_in_index or unk_idx is not None]
-#     return indices
-
-
-# def evaluate(input_seq,max_len=30):
-#     with torch.no_grad():
-#         input_tensor=torch.tensor(process_input(input_seq),dtype=torch.long).unsqueeze(0).to(device)
-#         start_indx=word_out_index['<SOS>']
-#         decoder_input=torch.tensor([[start_indx]],dtype=torch.long).to(device)
-#         input_embedding=model.input_embedding(input_tensor)
-#         _,(hidden,cell)=model.encoder(input_embedding)
-#         output=[]
-#         for _ in range(max_len):
-#             decoder_embedded = model.output_embedding(decoder_input)
-#             decoder_output, (hidden, cell) = model.decoder(decoder_embedded, (hidden, cell))
-#             prediction = model.Linear(decoder_output.squeeze(1))  # [1, vocab_size]
-
-#             top1 = prediction.argmax(1).item()
-#             output.append(top1)
-
-#             if top1 == word_out_index["<EOS>"]:
-#                 break
-#             decoder_input = torch.tensor([[top1]], dtype=torch.long).to(device)
-        
-#     decoded_sentence = [index_out_word[idx] for idx in output if idx in index_out_word]
-#     return decoded_sentence
-# while True:
-#         user_message=input("Your message:")
-#         response=evaluate(user_message)
-#         print('Virtual assistant:', ''.join(response))
-
 from train import VoiceModel, device
 import torch
 import nltk
 from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
 
-# nltk.download("punkt")
-# nltk.download("wordnet")
+
 
 word_mapping = torch.load('word_dict.pth')
 word_in_index = word_mapping['word_in_index']
@@ -122,7 +50,7 @@ def evaluate(input_seq, max_len=30):
             output_ids.append(top1)
             decoder_input = torch.tensor([[top1]], dtype=torch.long).to(device)
 
-    # 🔑 Clean up the sentence
+    # Clean up the sentence
     decoded_sentence = [
         index_out_word[idx] for idx in output_ids
         if idx in index_out_word and index_out_word[idx] not in ["<EOS>", "<PAD>", "<SOS>"]
